496c08625fa527f84ac1226e0e3974ecc7408c4a
Alex Cole <jac0038@uah.edu>
2023-10-06 22:13:19 +0000

    changes

--- webjar-maven-plugin/src/main/java/com/example/demo/NodeModulesMojo.java
+++ webjar-maven-plugin/src/main/java/com/example/demo/NodeModulesMojo.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Logger;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
@@ -13,8 +14,10 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-@Mojo(name = "CopyNodeModules", defaultPhase = LifecyclePhase.VALIDATE)
+@Mojo(name = "copyNodeModules", defaultPhase = LifecyclePhase.INITIALIZE)
 public class NodeModulesMojo extends AbstractMojo {
+
+    private static final Logger logger = Logger.getLogger(NodeModulesMojo.class.getName());
 
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     MavenProject project;
@@ -24,15 +27,21 @@ public class NodeModulesMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+        logger.info("Running execute");
+        getDependencies();
+        makeNodeModules();
     }
 
 
     public void getDependencies() {
+        logger.info(project.getDependencies().toString());
+
+        var dependencies = project.getDependencies();
+
+        
         Set<Artifact> allArtificats = project.getArtifacts();
         List<Artifact> webjarArtifacts = new LinkedList<>();
-
+        logger.info(allArtificats.toString());
         allArtificats.forEach(artifact -> {
             if (artifact.getGroupId().contains("org.webjars")) {
                 webjarArtifacts.add(artifact);
@@ -45,6 +54,12 @@ public class NodeModulesMojo extends AbstractMojo {
             }
         });
 
+    }
+
+    public void makeNodeModules() {
+        File file = new File();
+        // verify if node modules is there
+        // If not there, make it there
     }
 
 }
